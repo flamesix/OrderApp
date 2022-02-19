b5ea013afe603118d047437988ff7fadd9431130
grinevich <grinevichyu@gmail.com>
2022-02-19 10:07:09 +0300

    added models

---
+++ OrderApp/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  OrderApp
+//
+//  Created by Юрий Гриневич on 19.02.2022.
+//
+
+import Foundation
---
+++ OrderApp/ResponseModels.swift
@@ -0,0 +1,8 @@
+//
+//  ResponseModels.swift
+//  OrderApp
+//
+//  Created by Юрий Гриневич on 19.02.2022.
+//
+
+import Foundation
